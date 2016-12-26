750fdb61f3e8f1b48f47e2da3fc5f39cb12a4d88
David Falcão <davidrsfalcao@gmail.com>
2016-12-26 19:03:41 +0000

    update para a versão final do projeto anterior

--- src/aula.cpp
+++ src/aula.cpp
@@ -1,7 +1,7 @@
 #include "aula.h"
 
 using namespace std;
-unsigned int Aula::idloc=0;
+unsigned int Aula::idloc = 0;
 
 Aula::Aula()
 {
@@ -10,20 +10,20 @@ Aula::Aula()
 
 Aula::~Aula()
 {
-	for (size_t i=0; i <utentes.size();i++){
-				delete(utentes[i]);
-			}
+	for (size_t i = 0; i <utentes.size(); i++) {
+		delete(utentes[i]);
+	}
 }
 
 
-Aula::Aula(Data &inicio, Data & fim,int & duracao,int & NumMaximo)
+Aula::Aula(Data &inicio, Data & fim, int & duracao, int & NumMaximo)
 {
-	this->inicio=inicio;
-	this->fim=fim;
-	this->duracao=duracao;
-	this->NumMaximo=NumMaximo;
+	this->inicio = inicio;
+	this->fim = fim;
+	this->duracao = duracao;
+	this->NumMaximo = NumMaximo;
 	idloc++;
-	id=idloc;
+	id = idloc;
 
 }
 
@@ -39,12 +39,12 @@ void Aula::addUtente(Utente *ut)
 
 void Aula::removeUtente(int i)
 {
-	utentes.erase(utentes.begin()+i);
+	utentes.erase(utentes.begin() + i);
 }
 
 void Aula::setProfessor(Professor *prof)
 {
-	this->professor=(*prof);
+	this->professor = (*prof);
 }
 
 unsigned int Aula::getnumaula()
@@ -84,28 +84,28 @@ bool AulaPro::pro()
 	return true;
 }
 
-float Aula::price(){
-	return 0;
+float Aula::price() {
 
+	return 0;
 }
 
 float AulaLivre::price()
 {
-	float preco=0;
-	float precofinal=0;
+	float preco = 0;
+	float precofinal = 0;
 
 	if ((inicio.getHoras() >= 17) && (inicio.getHoras() <= 21))
 	{
 		preco = 4;
-		precofinal=(duracao/30)*preco;
+		precofinal = (duracao / 30)*preco;
 		return precofinal;
 
 	}
 	else
 	{
-	preco = 3.5;
-	precofinal=(duracao/30)*preco;
-	return precofinal;
+		preco = 3.5;
+		precofinal = (duracao / 30)*preco;
+		return precofinal;
 	}
 }
 
@@ -118,7 +118,7 @@ float AulaPro::price()
 
 unsigned int Aula::getId()
 {
-return id;
+	return id;
 }
 
 void Aula::setInicio(Data &d1)
@@ -144,27 +144,27 @@ void Aula::setNumMaximo(unsigned int max)
 void Aula::setId()
 {
 	idloc++;
-	id=idloc;
+	id = idloc;
 }
 
-void Aula::setId(unsigned int id){
-this->id=id;
+void Aula::setId(unsigned int id) {
+	this->id = id;
 }
 
-ostream & operator <<(ostream &of, const Aula &a1){
-	of<<"ID:"<< a1.id<<endl<<a1.inicio<<endl <<a1.fim <<endl<<"Professor:"<<a1.professor<< endl << "Incritos na aula:"<<endl;
-for(size_t i=0; i<a1.utentes.size();i++){
-	of<< a1.utentes[i]->getNome()<<endl;
-}
+ostream & operator <<(ostream &of, const Aula &a1) {
+	of << "ID:" << a1.id << endl << a1.inicio << endl << a1.fim << endl << "Professor:" << a1.professor << endl << "Incritos na aula:" << endl;
+	for (size_t i = 0; i<a1.utentes.size(); i++) {
+		of << a1.utentes[i]->getNome() << endl;
+	}
 	return of;
 }
 
 
- bool operator <(Aula &a1, Aula &a2){
-	return a1.getInicio() < a2.getInicio();
+bool Aula::operator <(const Aula &a1) {
+	return inicio < a1.getInicio();
 }
 
 
- Professor Aula::getProfessor(){
-return professor;
- }
+Professor Aula::getProfessor() {
+	return professor;
+}
--- src/aula.h
+++ src/aula.h
@@ -15,7 +15,7 @@ class Utente;
 
 using namespace std;
 
-class Aula{
+class Aula {
 protected:
 	Data inicio;
 	Data fim;
@@ -28,173 +28,173 @@ protected:
 
 public:
 	/**
-	 * @brief construtor da classe aula
-	 */
+	* @brief construtor da classe aula
+	*/
 	Aula();
 	/**
-	 * @brief Construtor da classe aula com parametros
-	 *
-	 * @param inicio data do Inicio da aula
-	 * @param fim data do Fim da aula
-	 * @param duracao duracao da aula
-	 * @param NumMaximo numero maximo de utentes da aula
-	 */
-	Aula(Data &inicio, Data & fim,int & duracao,int & NumMaximo);
+	* @brief Construtor da classe aula com parametros
+	*
+	* @param inicio data do Inicio da aula
+	* @param fim data do Fim da aula
+	* @param duracao duracao da aula
+	* @param NumMaximo numero maximo de utentes da aula
+	*/
+	Aula(Data &inicio, Data & fim, int & duracao, int & NumMaximo);
 	/**
-	 * @brief Destrutor da classe aula
-	 */
-	 ~Aula();
+	* @brief Destrutor da classe aula
+	*/
+	~Aula();
 	/**
-	 * @return Retorna a data do Incicio de uma aula
-	 */
+	* @return Retorna a data do Incicio de uma aula
+	*/
 	Data getInicio() const;
 	/**
-	 * @return Retorna a Duracao de uma aula
-	 */
+	* @return Retorna a Duracao de uma aula
+	*/
 	int getDuracao();
 	/**
-	 * @return Retorna a data do fim de uma aula
-	 */
+	* @return Retorna a data do fim de uma aula
+	*/
 	Data getFim();
 	/**
-	 * @return retorna o numero maximo de uma aula
-	 */
+	* @return retorna o numero maximo de uma aula
+	*/
 	int getNumMaximo();
 	/**
-	 * @return Retorna o numero de utentes numa aula
-	 */
+	* @return Retorna o numero de utentes numa aula
+	*/
 	unsigned int getnumaula();
 	/**
-	 * @brief diz se uma aula é livre ou de treino
-	 *
-	 * @return True se for Treino,False se Aula Livre
-	 */
-	virtual bool pro() { return true; };
+	* @brief diz se uma aula é livre ou de treino
+	*
+	* @return True se for Treino,False se Aula Livre
+	*/
+	virtual bool pro() { return true; }
 	/**
-	 * @return o preço a pagar por aula
-	 */
+	* @return o preço a pagar por aula
+	*/
 	virtual float price();
 	/**
-	 * @brief Define um professor para a aula de treino
-	 *
-	 * @param prof Professor que se pretende atribuir
-	 */
-	void setProfessor( Professor *prof);
+	* @brief Define um professor para a aula de treino
+	*
+	* @param prof Professor que se pretende atribuir
+	*/
+	void setProfessor(Professor *prof);
 	/**
-	 * @return retorna um vetor com os utentes da aula
-	 */
+	* @return retorna um vetor com os utentes da aula
+	*/
 	vector<Utente *> getUtentes();
 	/**
-	 * @brief Adiciona um utente a aula
-	 *
-	 * @param ut Utente que vai ser adicionado
-	 */
+	* @brief Adiciona um utente a aula
+	*
+	* @param ut Utente que vai ser adicionado
+	*/
 	void addUtente(Utente *ut);
 	/**
-	 * @brief remove um utente da aula
-	 *
-	 * @param i id do utente que vai ser removido
-	 */
+	* @brief remove um utente da aula
+	*
+	* @param i id do utente que vai ser removido
+	*/
 	void removeUtente(int i);
 	/**
-	 * @return Retorna o Id do utente
-	 *
-	 */
+	* @return Retorna o Id do utente
+	*
+	*/
 	unsigned int getId();
 	/**
-	 * @brief Define um Inicio para uma aula
-	 *
-	 * @param d1 a data que vai ser definida
-	 */
+	* @brief Define um Inicio para uma aula
+	*
+	* @param d1 a data que vai ser definida
+	*/
 	void setInicio(Data &d1);
 	/**
-	 * @brief Define um Fim para uma aula
-	 *
-	 * @param d1 a data que vai ser definida
-	 */
+	* @brief Define um Fim para uma aula
+	*
+	* @param d1 a data que vai ser definida
+	*/
 	void setFim(Data &d1);
 	/**
-	 * @brief Define a duracao de uma aula
-	 *
-	 * @param d duracao que vai ser definida
-	 */
+	* @brief Define a duracao de uma aula
+	*
+	* @param d duracao que vai ser definida
+	*/
 	void setDuracao(unsigned int d);
 	/**
-	 * @brief Define um Numero Maximo para uma aula
-	 *
-	 * @param max o numero maximo que vai ser definido
-	 */
+	* @brief Define um Numero Maximo para uma aula
+	*
+	* @param max o numero maximo que vai ser definido
+	*/
 	void setNumMaximo(unsigned int max);
 	/**
-	 * @brief Define um id para uma aula nova
-	 */
+	* @brief Define um id para uma aula nova
+	*/
 	void setId();
 	/**
-	 * @brief Define um id para uma aula
-	 *
-	 * @param id Id que vai ser atribuido
-	 */
+	* @brief Define um id para uma aula
+	*
+	* @param id Id que vai ser atribuido
+	*/
 	void setId(unsigned int id);
 	/**
-	 * @brief para mostrar uma aula
-	 *
-	 * @param of ostream para mostrar a aula
-	 * @param Aula aula que vai ser mostrada
-	 */
+	* @brief para mostrar uma aula
+	*
+	* @param of ostream para mostrar a aula
+	* @param Aula aula que vai ser mostrada
+	*/
 	friend ostream & operator <<(std::ostream &of, const Aula &a1);
 
 	/**
-		 * @brief operador que compara as aulas consoante a sua data de in�cio
-		 *
-		 * @param aula 1 a comparar
-		 * @param aula 2 a comparar
-		 */
-	friend bool operator <(const Aula &a1, const Aula &a2);
+	* @brief operador que compara as aulas consoante a sua data de in�cio
+	*
+	* @param aula 1 a comparar
+	* @param aula 2 a comparar
+	*/
+	bool operator <(const Aula &a1);
 
 	/**
-		 *  @brief fun��o que retorna o professor da aula
-		 *
-		 *  @return Professor
-		 */
-		Professor getProfessor();
+	*  @brief fun��o que retorna o professor da aula
+	*
+	*  @return Professor
+	*/
+	Professor getProfessor();
 
-		void setIdloc(unsigned int id){this->idloc=id;};
+	void setIdloc(unsigned int id) { this->idloc = id; };
 
 };
 
-class AulaLivre:public Aula{
+class AulaLivre :public Aula {
 public:
 	/**
-	 * @brief Construtor da Aula Livre
-	 */
-	AulaLivre():Aula(){};
+	* @brief Construtor da Aula Livre
+	*/
+	AulaLivre() :Aula() {};
 	/**
-	 * @brief diz se uma aula é livre ou de treino
-	 *
-	 * @return True se for Treino,False se Aula Livre
-	 */
+	* @brief diz se uma aula é livre ou de treino
+	*
+	* @return True se for Treino,False se Aula Livre
+	*/
 	bool pro();
 	/**
-	 * @return o preço a pagar por aula
-	 */
+	* @return o preço a pagar por aula
+	*/
 	float price();
 };
 
-class AulaPro:public Aula{
+class AulaPro :public Aula {
 public:
 	/**
-	 * @brief Construtor da Aula de treino
-	 */
-	AulaPro():Aula(){};
+	* @brief Construtor da Aula de treino
+	*/
+	AulaPro() :Aula() {};
 	/**
-	 * @brief diz se uma aula é livre ou de treino
-	 *
-	 * @return True se for Treino,False se Aula Livre
-	 */
+	* @brief diz se uma aula é livre ou de treino
+	*
+	* @return True se for Treino,False se Aula Livre
+	*/
 	bool pro();
 	/**
-	 * @return o preço a pagar por aula
-	 */
+	* @return o preço a pagar por aula
+	*/
 	float price();
 };
 
