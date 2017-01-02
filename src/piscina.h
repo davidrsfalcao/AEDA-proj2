b42bcbcff74594e5bb3a3a3901cad573d3a7194f
Pedro Miranda <pedro21fcp@gmail.com>
2017-01-02 16:19:30 +0000

    piscina.h

--- src/piscina.h
+++ src/piscina.h
@@ -271,67 +271,147 @@ public:
 	*
 	*/
 	bool aulaSobreposta(Aula *a1);
-
+	/**
+	* @brief mostra os detelhes da aula
+	*/
 	void detalhes_aula();
-
+	/**
+	* @brief mostra a frequencia do utente
+	*/
 	void freq_Utente();
-
+	/**
+	*   @brief atribui um professor a uma aula
+	*	@param a aula a ser atribuida
+	*/
 	void atribui_profaula(Aula *a);
-
+	/**
+	* @return retorna os utentes na piscina
+	*/
 	vector<Utente *> getutentes_piscina() { return utentesNaPisicina; };
-
+	/**
+	* @brief adiciona um utente a piscina
+	* @param u1 utente a ser adicionado
+	*/
 	void add_utente_napiscina(Utente* u1) { utentesNaPisicina.push_back(u1); };
-
+	/**
+	* @brief remove um utente a piscina
+	* @param u1 utente a ser removido
+	*/
 	void remove_utente_napiscina(Utente* u1);
-
+	/**
+	* @brief apaga uma aula
+	*/
 	void apaga_aulas();
-
+	/**
+	*   @brief paga a mensalidade de um utente
+	*   @param u1 utente que vai pagar a sua mensalidade
+	*/
 	void paga_mensalidade(Utente * u1);
-
+	/**
+	*	@brief adiciona um utente a uma aula
+	*	@param a aula a que vai ser adicionado o utente
+	*	@param u utente a ser adicionado
+	*/
 	void adiciona_utente_aula(Aula *a, Utente *u);
-
+	/**
+	*   @brief ordena os professores
+	*/
 	void ordena_professores();
-
+	/**
+	*   @brief mostra os detalhes do professore
+	*/
 	void detalhes_professor();
-
+	/**
+	*   @brief ordena os utentes
+	*/
 	void ordena_utentes();
-
+	/**
+	*   @brief compara a proximidade de duas piscinas
+	*	@param p1 piscina a ser comparada
+	*	@return True se a proximidade for menos,False se não for
+	*/
 	bool operator <(const Piscina &p1)const;
-
+	/**
+	*	@return retorna a posicao x da piscina
+	*/
 	unsigned int getX() const { return x; };
-
+	/**
+	*	@return retorna a posicao y da piscina
+	*/
 	unsigned int getY() const { return y; };
-
+	/**
+	* @return retorna a proximidade de uma piscina
+	*/
 	unsigned int getProximidade()const { return proximidade; };
-
+	/**
+	*	@brief define a proximidade de uma piscina
+	*   @param p proximidade a ser definida
+	*/
 	void setProximidade(unsigned int p) { this->proximidade = p; };
-
+	/**
+	*   @brief retira um utente dos inativos
+	*   @param u1 utente a ser retirado
+	*	@return retorn True se removido,False se não
+	*/
 	bool retira_inativo(const Utente &u1);
-
+	/**
+	*	@brief coloca utentes nos inativos
+	*   @return o numero de clientes colocados
+	*/
 	unsigned int coloca_inativos();
-
+	/**
+	*	@return retorna as modalidades disponiveis na piscina
+	*/
 	vector<string> getModalidades()const;
-
+	/**
+	*	@brief define as modalidades disponiveis na piscina
+	*	@param mod modalidades a ser definidas
+	*/
 	void setModalidades(vector<string> mod);
-	
+	/**
+	*   @brief atualiza as informacoes de um cliente inativo
+	*/
 	void campanha_promocional();
-
+	/**
+	*	@brief define a posicao x da piscina
+	*	@param x1 x a ser definido
+	*/
 	void setX(unsigned int x1);
-
+	/**
+	*	@brief define a posicao y da piscina
+	*	@param y1 y a ser definido
+	*/
 	void setY(unsigned int y1);
-
+	/**
+	*	@return retorna as piscinas mais proximas
+	*/
 	priority_queue<Piscina> getPiscinas_prox()const { return piscinas_prox; };
-
+	/**
+	*	@brief define as piscinas mais proximas
+	*	@param piscinas piscinas que vao ser definidas
+	*/
 	void setPiscinas_prox(const vector<Piscina> & piscinas);
-
+	/**
+	*	@param modalidade modalidade desejada pelo utente
+	*	@return retorna a piscina mais proxima com a modalidade pretendida
+	*/
 	Piscina getMaisperto(string modalidade);
-
+	/**
+	*	@brief imprime as piscinas mais proximas
+	*/
 	void imprime_piscinas_prox();
-
+	/**
+	*	@return retorna os utentes inativos
+	*/
 	tabHU getInativos() const { return utentes_inativos; };
-
+	/**
+	*	@return retorna a loja da piscina
+	*/
 	Loja getLoja() const { return loja_pis; };
-
+	/**
+	*	@brief define  a loja da piscina
+	*	@param lj1 loja que vai ser definida
+	*/
 	void setLoja(Loja lj1) { loja_pis = lj1; };
 
 };
